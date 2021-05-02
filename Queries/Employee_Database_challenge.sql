4a3622b3c241ec258d0f80bc67cf23bd0277b40c
Corinne <hume.corinne@gmail.com>
2021-05-02 16:15:27 -0700

    adding updated sql file

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -48,3 +48,38 @@ FROM employees as e
 WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 AND (de.to_date = ('9999-01-01'))
 ORDER BY e.emp_no, de.to_date;
+
+--Retrieve number of mentor eligible employees by most recent job title 
+SELECT COUNT(title),
+  me.title
+INTO mentorship_info
+FROM mentorship_eligibility as me
+GROUP BY me.title
+ORDER BY COUNT DESC;
+
+--Create mentorship_hire_date table for longest working employees eligible for mentorship program 
+SELECT DISTINCT ON (emp_no) e.emp_no,
+    e.first_name,
+	  e.last_name,
+    e.birth_date,
+    de.hire_date,
+    de.to_date,
+    ti.title
+    
+INTO mentorship_hire_date
+FROM employees as e
+  INNER JOIN dept_emp as de
+    ON(e.emp_no = de.emp_no)
+  INNER JOIN titles as ti
+    ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = ('9999-01-01'))
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+ORDER BY e.emp_no, de.to_date;
+--query table to find number of longest working employees eligible for mentorship program
+SELECT COUNT(mh.emp_no),
+  mh.title
+FROM mentorship_hire_date as mentorship_hire_date
+GROUP BY mh.title;
+
+
